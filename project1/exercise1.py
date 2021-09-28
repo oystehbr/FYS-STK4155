scaling and bais fixed

project1/exercise1.py
@@ -50,40 +50,31 @@ def generate_data(n, noise_multiplier=0.1):
     x_array = np.zeros(n)
     y_array = np.zeros(n)
     for i in range(n):
-        x = np.random.uniform(0, 1)
-        y = np.random.uniform(0, 1)
+        x_array[i] = np.random.uniform(0, 1)
+        y_array[i] = np.random.uniform(0, 1)
         eps = np.random.normal(0, 1)
-        z = franke_function(x, y) + noise_multiplier * eps
-        x_array[i] = x
-        y_array[i] = y
-        data_array[i] = z
-
-    # data_array = np.zeros(n)
-    # x = np.linspace(-3, 3, n).reshape(-1, 1)
-    # y = np.zeros(n)
-    # z = np.exp(-x**2) + 1.5 * np.exp(-(x-2)**2) + \
-    #     np.random.normal(0, 1, x.shape)
-    # return x, y, z
+        data_array[i] = franke_function(
+            x_array[i], y_array[i]) + noise_multiplier * eps
 
     return x_array, y_array, data_array
 
 
-def get_betas_OLS(X_train, z_values, degree=2):
+def get_betas_OLS(X, z_values, degree=2):
     """
     TODO: docstrings
 
 
     """
 
-    X_train_T = np.matrix.transpose(X_train)
-    betas = np.linalg.pinv(X_train_T @ X_train) @ X_train_T @ z_values
+    X_T = np.matrix.transpose(X)
+    betas = np.linalg.pinv(X_T @ X) @ X_T @ z_values
     # LÆRER SA DENNE VAR BEST:
     # TODO: sjekk denne metoden for å regne betas
     # beta = np.linalg.pinv(X_T @ X) @ X_T @ z_values -> SVD
     return betas
 
 
-def scale_design_matrix(X):
+def scale_design_matrix(X, mean_scale):
     """
     Scaling the desingmatrix by subtracting the mean of each column
 
@@ -94,8 +85,7 @@ def scale_design_matrix(X):
         the scaled matrix
     """
 
-    X_mean = np.mean(X, axis=0)
-    X_scaled = X - X_mean
+    X_scaled = X - mean_scale
     return X_scaled
 
 
@@ -175,10 +165,13 @@ def main(n=1000, degree=5, test_size=0.2, noise=0):
     X_test = helper.create_design_matrix(x_test, y_test, degree)
 
     # Scale data before further use
-    X_train_scaled = scale_design_matrix(X_train)
-    X_test_scaled = scale_design_matrix(X_test)
-    z_test_scaled = scale_design_matrix(z_test)
-    z_train_scaled = scale_design_matrix(z_train)
+    X_train_scale = np.mean(X_train, axis=0)
+    X_train_scaled = X_train - X_train_scale
+
+    # TODO: shall we scale with the above?
+    X_test_scaled = X_test - X_train_scale
+    z_train_scale = np.mean(z_train, axis=0)
+    z_train_scaled = z_train - z_train_scale
 
     # Get the betas from OLS.
     betas_OLS = get_betas_OLS(X_train_scaled, z_train_scaled)
@@ -190,7 +183,7 @@ def main(n=1000, degree=5, test_size=0.2, noise=0):
     # print_betas_CI(betas_OLS, CI_list)
 
     # Scale the data back to its original form
-    z_pred_test = z_predicted(X_test_scaled, betas_OLS) + np.mean(z_train)
+    z_pred_test = z_predicted(X_test_scaled, betas_OLS) + z_train_scale
 
     # Evaluating the Mean Squared error (MSE)
     # TODO: create function for getting MSE and R2_score at same time

project1/exercise2.py
@@ -41,7 +41,6 @@ def bias_variance_boots(x_values, y_values, z_values, max_degree=10, test_size=0
 
         # TODO: comments
         X_test = helper.create_design_matrix(x_test, y_test, degree)
-        X_test_scaled = exercise1.scale_design_matrix(X_test)
         # Running bootstrap-method on training data -> collect the different betas
         for i in range(n_bootstrap):
 
@@ -49,29 +48,39 @@ def bias_variance_boots(x_values, y_values, z_values, max_degree=10, test_size=0
 
             # Train the model with the training data
             X_train = helper.create_design_matrix(_x, _y, degree)
-            X_train_scaled = exercise1.scale_design_matrix(X_train)
+            X_train_scale = np.mean(X_train, axis=0)
+            X_train_scaled = X_train - X_train_scale
 
-            z_train_scaled = exercise1.scale_design_matrix(_z)
+            X_test_scaled = X_test - X_train_scale
+            _z_scale = np.mean(_z, axis=0)
+            _z_scaled = _z - _z_scale
 
             # Evaluate the new model on the same test data each time.
             betas_OLS = exercise1.get_betas_OLS(
-                X_train_scaled, z_train_scaled)
+                X_train_scaled, _z_scaled)
 
             # Finding the predicted z values with the current model
             z_pred = exercise1.z_predicted(
-                X_test_scaled, betas_OLS) + np.mean(_z)
+                X_test_scaled, betas_OLS) + _z_scale
 
-            z_pred_test_matrix[:, i] = z_pred.ravel()
+            z_pred_test_matrix[:, i] = z_pred
 
-        MSE_test = np.mean(
-            np.mean((z_test - z_pred_test_matrix)**2, axis=1, keepdims=True))
+        # TODO: explain this, maybe vectorize
+        MSE_test = 0
+        n = len(z_test)
+        for j in range(n):
+            for i in range(n):
+
+                MSE_test += (z_pred_test_matrix[j][i] - z_test[j])**2
+
+        MSE_test *= 1/(n**2)
 
         # TODO: check bias
         BIAS_test = np.mean(
-            (z_test - np.mean(z_pred_test_matrix, axis=1, keepdims=True))**2)
+            (z_test - np.mean(z_pred_test_matrix, axis=1, keepdims=False))**2)
 
         variance_test = np.mean(
-            np.var(z_pred_test_matrix, axis=1, keepdims=True))
+            np.var(z_pred_test_matrix, axis=1, keepdims=False))
 
         list_of_MSE_testing.append(MSE_test)
         list_of_BIAS_testing.append(BIAS_test)
@@ -115,30 +124,29 @@ def plot_MSE_vs_complexity(x_values, y_values, z_values, max_degree=10, test_siz
     for degree in range(1, max_degree + 1):
         # Get designmatrix from the training data and scale it
         X_train = helper.create_design_matrix(x_train, y_train, degree)
-        X_train_scaled = exercise1.scale_design_matrix(X_train)
+        X_train_scale = np.mean(X_train, axis=0)
+        X_train_scaled = X_train - X_train_scale
 
         # Get designmatrix from the test data and scale it
         X_test = helper.create_design_matrix(x_test, y_test, degree)
-        X_test_scaled = exercise1.scale_design_matrix(X_test)
+        X_test_scaled = X_test - X_train_scale
 
         # Scale the output_values
-        z_train_scaled = exercise1.scale_design_matrix(z_train)
+        z_train_scale = np.mean(z_train, axis=0)
+        z_train_scaled = z_train - z_train_scale
 
         # Get the betas from OLS - method
         betas_OLS = exercise1.get_betas_OLS(X_train_scaled, z_train_scaled)
 
         # Find out how good the model is on our test data
-        X_test = helper.create_design_matrix(x_test, y_test, degree)
-
-        # Find out how good the model is on our test data
         z_pred_test = exercise1.z_predicted(
-            X_test_scaled, betas_OLS) + np.mean(z_train)
+            X_test_scaled, betas_OLS) + z_train_scale
         MSE_test = mean_squared_error(z_test, z_pred_test)
         list_of_MSE_testing.append(MSE_test)
 
         # Find out how good the model is on our training data
         z_pred_train = exercise1.z_predicted(
-            X_train_scaled, betas_OLS) + np.mean(z_train)
+            X_train_scaled, betas_OLS) + z_train_scale
         MSE_train = mean_squared_error(z_train, z_pred_train)
         list_of_MSE_training.append(MSE_train)
 
@@ -158,16 +166,22 @@ def plot_MSE_vs_complexity(x_values, y_values, z_values, max_degree=10, test_siz
     plt.close()
 
 
-def main(n=20, noise=0.1):
+def main(n=1000, noise=0.2):
 
     x_values, y_values, z_values = exercise1.generate_data(n, noise)
 
-    bias_variance_boots(x_values, y_values, z_values,
-                        max_degree=5, test_size=0.2)
+    # n = 200, noise = 0.0 - 0.1, max_degree = 8 -> great bias_variance
+    # bias_variance_boots(x_values, y_values, z_values,
+    #                     max_degree=8, test_size=0.2, show_plot=True)
 
-    # plot_MSE_vs_complexity(x_values, y_values, z_values,
-    #                        max_degree=20, test_size=0.2)
+    plot_MSE_vs_complexity(x_values, y_values, z_values,
+                           max_degree=20, test_size=0.2)
 
 
 if __name__ == '__main__':
     main()
+
+    # for i in range(300, 600, 50):
+    #     for j in range(5):
+    #         print(0.2*j)
+    #         main(n=i, noise=0.2*j)

project1/exercise3.py
@@ -92,12 +92,14 @@ def cross_validation(x_values, y_values, z_values, k_folds=5, degree=5):
         # TODO: what do we want with this
         # Train the model with the training data
         X_train = helper.create_design_matrix(x_train, y_train, degree)
-        X_train_scaled = exercise1.scale_design_matrix(X_train)
+        X_train_scale = np.mean(X_train, axis=0)
+        X_train_scaled = X_train - X_train_scale
 
-        z_train_scaled = exercise1.scale_design_matrix(z_train)
+        z_train_scale = np.mean(z_train, axis=0)
+        z_train_scaled = z_train - z_train_scale
 
         X_test = helper.create_design_matrix(x_test, y_test, degree)
-        X_test_scaled = exercise1.scale_design_matrix(X_test)
+        X_test_scaled = X_test - X_train_scale
 
         # Evaluate the new model on the same test data each time.
         betas_OLS = exercise1.get_betas_OLS(
@@ -105,7 +107,7 @@ def cross_validation(x_values, y_values, z_values, k_folds=5, degree=5):
 
         # Finding the predicted z values with the current model
         z_pred = exercise1.z_predicted(
-            X_test_scaled, betas_OLS) + np.mean(z_train)
+            X_test_scaled, betas_OLS) + z_train_scale
 
         MSE_list[i] = exercise1.mean_squared_error(z_test, z_pred)
 

project1/test.py
@@ -2,7 +2,9 @@ import numpy as np
 
 
 a = np.array([1, 2, 3])
-b = np.array([5, 3])
+b = np.array([[5, 3], [3, 5]])
+
+print(np.mean(4))
 
 c = np.concatenate((a, b))
 