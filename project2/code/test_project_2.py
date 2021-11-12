a90d66ddddb35c00f407627a6191ddd890580e1f
oystehbr <oysteinrbk@hotmail.com>
2021-11-12 14:04:14 +0100

    small changes in description names to the tests

--- project2/code/test_project_2.py
+++ project2/code/test_project_2.py
@@ -482,6 +482,8 @@ if test6:
 
 """
 TEST 7:
+DATASET: CANCER DATA (classification case)
+
 Logistic regression: create a seaborn plot of the hyperparameter lambda
 and the learning rate. Other SGD values, will be easy to change in the test.
 """
@@ -550,6 +552,8 @@ if test7:
 
 """
 TEST 8:
+DATASET: CANCER DATA (classification case)
+
 Comparison: logistic regression vs classification with Neural Network. Comparing
 the R2-score on testing and training data, and the time spent on training the model.
 """
